ef27ee07563050a4e944d2a5373c3218960fe00f
Daniel Stempfle <stempfledaniel@gmx.de>
2023-09-02 12:29:53 +0200

    Refactor Code

--- githubLandingPage/src/App.vue
+++ githubLandingPage/src/App.vue
@@ -1,12 +1,12 @@
 <template>
 <h1>My landing page</h1>
-  <ShaderBox shaderFile="/shaders/RayMarcher.txt" resolution-width="1920" resolution-height="1080"></ShaderBox>
+  <FragmentShaderBox shaderFile="/shaders/RayMarcher.txt" resolution-width="1920" resolution-height="1080"></FragmentShaderBox>
 
 </template>
 <script setup lang="ts">
 
 import {onMounted} from "vue";
-import ShaderBox from "./components/ShaderBox.vue";
+import FragmentShaderBox from "./components/FragmentShaderBox.vue";
 
 
 
