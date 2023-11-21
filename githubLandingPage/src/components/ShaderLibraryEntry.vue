2f77fcbb57cc52c1f1d65b4af42c08c070718ca6
kastanileel <stempfledaniel@gmx.de>
2023-11-21 22:12:24 +0100

    Adjust css in shader page

--- githubLandingPage/src/components/ShaderLibraryEntry.vue
+++ githubLandingPage/src/components/ShaderLibraryEntry.vue
@@ -4,16 +4,16 @@
         <iframe :src=link allowfullscreen width="100%" height="100%"></iframe>
       </div>
 
-      <div class="halfSide">
-        <div class="text">
+      <div class="halfSide" :style="{height: dynamicHeight}">
+        <div class="text" ref="textContent">
           <h2>{{ title }}</h2>
           <p>{{ description }}</p>
         </div>
       </div>
     </div>
     <div v-else class="shader-card" :style="{backgroundColor:color, color:textColor}">
-      <div class="halfSide">
-        <div class="text">
+      <div class="halfSide" :style="{height: dynamicHeight}">
+        <div class="text" ref="textContent">
           <h2>{{ title }}</h2>
           <p>{{ description }}</p>
         </div>
@@ -29,6 +29,14 @@
 import { defineProps } from 'vue';
 import {ref} from "vue";
 import {onMounted} from "vue";
+import {  watch } from 'vue';
+
+// ... existing code ...
+
+// Ref for the text content element
+const textContent = ref(null);
+// Ref for dynamic height
+const dynamicHeight = ref('600px');
 
 let color = ref("#B0B0B0")
 let textColor = ref("#303030")
@@ -45,7 +53,22 @@ onMounted(() => {
     color.value = "#303030"
     textColor.value = "#B0B0B0"
   }
+
+  updateHeight();
 })
+
+function updateHeight() {
+  if (textContent.value) {
+    const height = textContent.value.offsetHeight;
+    dynamicHeight.value = `${height}px`;
+  }
+}
+
+watch(() => [props.description, props.title], () => {
+  // Update height when the content changes
+  updateHeight();
+}, { immediate: true });
+
 
 </script>
 
@@ -58,6 +81,7 @@ onMounted(() => {
   
   .halfSide{
     width: 100%;
+    height: 200px;
   }
 
 }
@@ -68,6 +92,7 @@ onMounted(() => {
 
   .halfSide {
     width: 50%;
+    height: 250px;
   }
 }
 .shader-card {
